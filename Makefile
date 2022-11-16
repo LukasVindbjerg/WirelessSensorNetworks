810f2b3cd48dd53dce1b94d0d862f8367d813194
MaltheT <malthe@toettrup.dk>
2022-11-16 11:49:34 +0100

    Created transciever node

--- Makefile
+++ Makefile
@@ -3,6 +3,15 @@ all: $(CONTIKI_PROJECT)
 
 MAKE_NET = MAKE_NET_NULLNET
 
+
 CONTIKI = ..
+#include $(CONTIKI)/Makefile.include
+#TARGET_LIBFILES = -lm
+
+
+PLATFORMS_EXCLUDE = nrf52dk
+
+#use this to enable TSCH: MAKE_MAC = MAKE_MAC_TSCH
+MAKE_MAC ?= MAKE_MAC_CSMA
+MAKE_NET = MAKE_NET_NULLNET
 include $(CONTIKI)/Makefile.include
-TARGET_LIBFILES = -lm
---
+++ node.c
@@ -0,0 +1,75 @@
+
+/**
+ * \file
+ *         Program that tests the RSSI for channels 11-26  
+ * \author
+ *         Malthe Tøttrup <201907882@post.au.dk>
+ * 
+ *         $ make TARGET=sky distclean 
+ *         $ make TARGET=sky MOTES=/dev/ttyUSB0 node.upload login
+ */
+
+#include "contiki.h"
+#include <stdio.h> /* For printf() */
+#include <math.h>
+#include <os/dev/radio.h>  
+#include "net/nullnet/nullnet.h"
+#include "net/netstack.h"
+#include <string.h>
+#include "sys/log.h"
+
+#define LOG_MODULE "App"
+#define LOG_LEVEL LOG_LEVEL_INFO
+#define SEND_INTERVAL (8 * CLOCK_SECOND)
+
+
+/*---------------------------------------------------------------------------*/
+PROCESS(hello_world_process, "Hello world process");
+AUTOSTART_PROCESSES(&hello_world_process);
+/*---------------------------------------------------------------------------*/
+
+void input_callback(const void *data, uint16_t len,
+  const linkaddr_t *src, const linkaddr_t *dest)
+{
+  if(len == sizeof(unsigned)) {
+    unsigned count;
+    memcpy(&count, data, sizeof(count));
+    LOG_INFO("Received %u from ", count);
+    LOG_INFO_LLADDR(src);
+    LOG_INFO_("\n");
+  }
+}
+
+PROCESS_THREAD(hello_world_process, ev, data)
+{   
+    PROCESS_BEGIN();
+
+    static unsigned count = 0;
+    static struct etimer et;
+
+    nullnet_buf = (uint8_t *)&count;
+    nullnet_len = sizeof(count);
+    nullnet_set_input_callback(input_callback);
+
+    static linkaddr_t mote186 =         {{ 0x34, 0xa3, 0xdf, 0x1c, 0x00, 0x74, 0x12, 0x00 }}; //Lukas
+    static linkaddr_t mote182 =         {{ 0x83, 0xac, 0xdf, 0x1c, 0x00, 0x74, 0x12, 0x00 }}; //Daniel
+    //static linkaddr_t mote118 =         {{ 0x5d, 0xe7, 0x93, 0x1c, 0x00, 0x74, 0x12, 0x00 }}; //Malthe
+
+    etimer_set(&et, SEND_INTERVAL);
+    while(1) {
+        PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&et));
+        LOG_INFO("Sending %u to ", count);
+        LOG_INFO_LLADDR(&mote186);
+        LOG_INFO_LLADDR(&mote182);
+        LOG_INFO_("\n");
+
+        NETSTACK_NETWORK.output(&mote186);
+        NETSTACK_NETWORK.output(&mote182);
+
+        count++;
+        etimer_reset(&et);
+    }
+
+    PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
